1ef3b6dd633e181c9dc74460ebba05e46a21b988
Thomas Traude <thomas.traude@codecentric.de>
2022-05-27 21:34:51 +0200

    Extend instructions for Navigation

--- KeyboardShortcutKatas/Navigation.py
+++ KeyboardShortcutKatas/Navigation.py
@@ -2,5 +2,6 @@ from KeyboardShortcutKatas._.__ import __
 
 
 def __main__():
-    # Start here navigating through the code using shortcuts, read the sentence
+    # Start here navigating through every function call using shortcuts,
+    # read the sentence build by the method names along the way.
     __.yesterday()
